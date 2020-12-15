9eff8d8c2eb714fef8a6d6730e8452b91cf23e4c
brizental <brizental@gmail.com>
2020-12-15 11:36:50 +0100

    Add jsdoc rules to linter and fix errors

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * Checks whether or not `v` is a simple data object.
+ *
+ * @param v The value to verify.
+ *
+ * @returns A special Typescript value (which compiles down to a boolean)
+ *          stating wether `v` is a valid data object.
+ */
 export function isObject(v: unknown): v is Record<string | number | symbol, unknown> {
   return (v && typeof v === "object" && v.constructor === Object);
 }
 
+/**
+ * Checks whether or not `v` is undefined.
+ *
+ * @param v The value to verify.
+ *
+ * @returns A special Typescript value (which compiles down to a boolean)
+ *          stating wether `v` is undefined.
+ */
 export function isUndefined(v: unknown): v is undefined {
   return typeof v === "undefined";
 }
 
+/**
+ * Checks whether or not `v` is a string.
+ *
+ * @param v The value to verify.
+ *
+ * @returns A special Typescript value (which compiles down to a boolean)
+ *          stating wether `v` is a string.
+ */
 export function isString(v: unknown): v is string {
   return (v && (typeof v === "string" || (v === "object" && v.constructor === String)));
 }
