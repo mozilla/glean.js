97ca6e5f2895d71080eb649e14777997a8bab12c
brizental <brizental@gmail.com>
2021-01-14 14:42:16 +0100

    Create an upload adapter

---
+++ src/upload/adapter/index.ts
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { JSONObject } from "utils";
+
+/**
+ * Uploader interface, actualy uploading logic varies per platform.
+ */
+export interface UploadAdapter {
+  /**
+   * Makes a POST request to a given url, with the given headers and body.
+   *
+   * @param url The URL to make the POST request
+   * @param options The optional headers and body to include in the request.
+   *
+   * @returns The status code of the response.
+   */
+  post(url: URL, options: {
+    headers?: Record<string, string>,
+    body: JSONObject
+  }): Promise<number>;
+}
+
+// Default export for tests sake.
+const MockUploadAdapter: UploadAdapter = {
+  post(): Promise<number> {
+    return Promise.resolve(200);
+  }
+};
+
+export default MockUploadAdapter;
