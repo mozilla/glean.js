53ec777ffbfe7be918ecc575a1d1f5bab99a9c09
brizental <brizental@gmail.com>
2021-01-14 14:41:36 +0100

    Add getAllPings and deletePing APIs to the pings database

--- tests/pings/database.spec.ts
+++ tests/pings/database.spec.ts
@@ -4,86 +4,228 @@
 
 import assert from "assert";
 
-import Database, { Observer } from "pings/database";
+import Database, { Observer, isValidPingInternalRepresentation } from "pings/database";
 
 describe("PingsDatabase", function() {
-  it("records correctly to the correct place at the underlying storage", async function() {
-    const db = new Database();
-    const path = "some/random/path/doesnt/matter";
-    const identifier = "THE IDENTIFIER";
-    const payload = {
-      ping_info: {
-        seq: 1,
-        start_time: "2020-01-11+01:00",
-        end_time: "2020-01-12+01:00",
-      },
-      client_info: {
-        telemetry_sdk_build: "32.0.0"
-      }
-    };
-    await db.recordPing(path, identifier, payload);
-    assert.deepStrictEqual(await db["store"].get([identifier]), {
-      path, payload
+
+  describe("record", function () {
+    it("records correctly to the correct place at the underlying storage", async function() {
+      const db = new Database();
+      const path = "some/random/path/doesnt/matter";
+      const identifier = "THE IDENTIFIER";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
+      await db.recordPing(path, identifier, payload);
+      assert.deepStrictEqual(await db["store"].get([identifier]), {
+        path, payload
+      });
+  
+      const headers = { "X-Debug-Id": "test" };
+      const otherIdentifier = "THE OTHER IDENTIFIER";
+      await db.recordPing(path, otherIdentifier, payload, headers);
+      assert.deepStrictEqual(await db["store"].get([otherIdentifier]), {
+        path, payload, headers
+      });
+  
+      assert.strictEqual(Object.keys(await db["store"]._getWholeStore()).length, 2);
     });
 
-    const headers = { "X-Debug-Id": "test" };
-    const otherIdentifier = "THE OTHER IDENTIFIER";
-    await db.recordPing(path, otherIdentifier, payload, headers);
-    assert.deepStrictEqual(await db["store"].get([otherIdentifier]), {
-      path, payload, headers
+    it("observer is notified when a new ping is added to the database", async function() {
+      let wasNotified = false;
+      const identifier = "THE IDENTIFIER";
+      const observer: Observer = {
+        update: (id: string): void => {
+          wasNotified = true;
+          assert.strictEqual(id, identifier);
+        }
+      };
+  
+      const db = new Database(observer);
+      const path = "some/random/path/doesnt/matter";
+
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
+      await db.recordPing(path, identifier, payload);
+      assert.ok(wasNotified);
+    });
+  });
+
+  describe("get", function () {
+    it("isValidPingInternalRepresentation validates correctly", function () {
+      // Invalid values
+      assert.strictEqual(isValidPingInternalRepresentation({}), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ path: "some/path" }), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: {} }), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: "not an object", path: "some/path" }), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: {}, path: 1234 }), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ headers: {} }), false);
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: {}, path: "some/path", headers: "not an object" }), false);
+
+      // Valid values
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: {}, path: "some/path", headers: {} }), true);
+      assert.strictEqual(isValidPingInternalRepresentation({ payload: {}, path: "some/path" }), true);
     });
 
-    assert.strictEqual(Object.keys(await db["store"]._getWholeStore()).length, 2);
+    it("when incorrect data is found on the storage it is deleted", async function () {
+      const db = new Database();
+      const identifier = "THE IDENTIFIER";
+      const path = "some/random/path/doesnt/matter";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
+      await db.recordPing(path, identifier, payload);
+
+      // Add weird stuff to the db
+      await db["store"].update(["incorrect"], () => "wrong data");
+      await db["store"].update(["more", "incorrectness"], () => "more wrong data");
+
+      const allPings = await db.getAllPings();
+      assert.strictEqual(Object.keys(allPings).length, 1);
+      assert.deepStrictEqual({
+        [identifier]: { path, payload }
+      }, allPings);
+    });
+
+    it("getAllPings works correct when data is all correct", async function () {
+      const db = new Database();
+      const path = "some/random/path/doesnt/matter";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
+
+      const identifiers = ["foo", "bar", "baz", "qux", "etc"];
+      identifiers.forEach(async (identifier) => {
+        await db.recordPing(path, identifier, payload);
+      });
+
+      const allPings = await db.getAllPings();
+      assert.strictEqual(Object.keys(allPings).length, 5);
+      identifiers.forEach(async (identifier, index) => {
+        assert.strictEqual(identifier, identifiers[index]);
+      });
+    });
+
+    it("getAllPings dosen't error when there are no pings stored", async function () {
+      const db = new Database();
+      assert.deepStrictEqual({}, await db.getAllPings());
+    });
   });
 
-  it("clearing works", async function() {
-    const db = new Database();
-    const path = "some/random/path/doesnt/matter";
-    const payload = {
-      ping_info: {
-        seq: 1,
-        start_time: "2020-01-11+01:00",
-        end_time: "2020-01-12+01:00",
-      },
-      client_info: {
-        telemetry_sdk_build: "32.0.0"
-      }
-    };
+  describe("delete", function() {
+    it("deleting works", async function() {
+      const db = new Database();
+      const path = "some/random/path/doesnt/matter";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
 
-    await db.recordPing(path, "foo", payload);
-    await db.recordPing(path, "bar", payload);
-    await db.recordPing(path, "baz", payload);
-    await db.recordPing(path, "qux", payload);
-    assert.strictEqual(Object.keys(await db["store"]._getWholeStore()).length, 4);
+      const identifiers = ["foo", "bar", "baz", "qux", "etc"];
+      identifiers.forEach(async (identifier) => {
+        await db.recordPing(path, identifier, payload);
+      });
 
-    await db.clearAll();
-    assert.strictEqual(Object.keys(await db["store"]._getWholeStore()).length, 0);
+      await db.deletePing("foo");
+      const allPings = await db.getAllPings();
+      assert.strictEqual(Object.keys(allPings).length, 4);
+      ["bar", "baz", "qux", "etc"].forEach(async (identifier, index) => {
+        assert.strictEqual(identifier, identifiers[index]);
+      });
+
+      await db.deletePing("bar");
+      const allPings2 = await db.getAllPings();
+      assert.strictEqual(Object.keys(allPings2).length, 3);
+      ["baz", "qux", "etc"].forEach(async (identifier, index) => {
+        assert.strictEqual(identifier, identifiers[index]);
+      });
+    });
+
+    it("deleting a ping that is not in the db doesn't error", async function() {
+      const db = new Database();
+      const path = "some/random/path/doesnt/matter";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
+
+      const identifiers = ["foo", "bar", "baz", "qux", "etc"];
+      identifiers.forEach(async (identifier) => {
+        await db.recordPing(path, identifier, payload);
+      });
+
+      await db.deletePing("no existo");
+      const allPings = await db.getAllPings();
+      assert.strictEqual(Object.keys(allPings).length, 5);
+      identifiers.forEach(async (identifier, index) => {
+        assert.strictEqual(identifier, identifiers[index]);
+      });
+    });
   });
 
-  it("observer is notified when a new ping is added to the database", async function() {
-    let wasNotified = false;
-    const identifier = "THE IDENTIFIER";
-    const observer: Observer = {
-      update: (id: string): void => {
-        wasNotified = true;
-        assert.strictEqual(id, identifier);
-      }
-    };
+  describe("clear", function () {
+    it("clearing works", async function() {
+      const db = new Database();
+      const path = "some/random/path/doesnt/matter";
+      const payload = {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      };
 
-    const db = new Database(observer);
-    const path = "some/random/path/doesnt/matter";
-
-    const payload = {
-      ping_info: {
-        seq: 1,
-        start_time: "2020-01-11+01:00",
-        end_time: "2020-01-12+01:00",
-      },
-      client_info: {
-        telemetry_sdk_build: "32.0.0"
-      }
-    };
-    await db.recordPing(path, identifier, payload);
-    assert.ok(wasNotified);
+      const identifiers = ["foo", "bar", "baz", "qux", "etc"];
+      identifiers.forEach(async (identifier) => {
+        await db.recordPing(path, identifier, payload);
+      });
+  
+      await db.clearAll();
+      assert.strictEqual(Object.keys(await db["store"]._getWholeStore()).length, 0);
+    });
   });
 });
