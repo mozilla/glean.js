03140c6a2404d11393cd86241aba57e0f10b3a96
brizental <brizental@gmail.com>
2021-02-05 15:53:09 +0100

    Update the web-extensio sample

--- samples/web-extension/index.js
+++ samples/web-extension/index.js
@@ -8,30 +8,20 @@ import Glean from "glean";
 import { samplePing } from "./generatedPings";
 import { webExtStarted, popupOpened } from "./generatedMetrics";
 
-// TODO: Do not wait for Glean to be initialized before recording metrics
-// once Bug 1687491 is resolved.
-Glean.initialize("web-extension", true)
-  .then(() => {
-    console.log("Glean has been succesfully initialized.");
-    webExtStarted.set()
-      .then(() => console.log("`webext-installed` was succesfully set."));
-  });
+
+Glean.initialize("web-extension", true);
+webExtStarted.set();
 
 // Listen for messages from the popup.
 browser.runtime.onMessage.addListener(msg => {
   console.log(`New message received! ${msg}`);
 
   if (msg === "popup-opened") {
-    popupOpened.add()
-      .then(() => console.log("`popup-opened` was succesfully added."));
+    popupOpened.add();
   }
 
   if (msg === "send-ping") {
-    samplePing.submit()
-      .then(wasSubmitted => {
-        console.log(
-          `Attempted to send ping "${samplePing.name}". Was the ping sent? ${wasSubmitted}`);
-      });
+    samplePing.submit();
   }
 });
 
