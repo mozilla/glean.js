243d9706a4b04dbc7ff5134df7256c150e3875ae
brizental <brizental@gmail.com>
2021-03-04 17:15:25 +0100

    First attempt at providing a ping encrypiton plugin

---
+++ glean/src/plugins/encryption.ts
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import CompactEncrypt from "jose/jwe/compact/encrypt";
+import parseJwk from "jose/jwk/parse";
+import { JWK } from "jose/types";
+
+import Plugin from "./index";
+import { PingPayload } from "../core/pings/database";
+import { JSONObject } from "../core/utils";
+import CoreEvents from "../core/events";
+
+/**
+ * A plugin that encrypts the payload of a ping before we store it.
+ */
+class PingEncryptionPlugin extends Plugin<typeof CoreEvents["afterPingCollection"]> {
+  constructor(readonly jwk: JWK) {
+    super(CoreEvents["afterPingCollection"].name, "pingEncryptionPlugin");
+  }
+
+  async action(payload: PingPayload): Promise<JSONObject> {
+    const key = await parseJwk(this.jwk);
+    const encoder = new TextEncoder();
+    const encodedPayload = await new CompactEncrypt(encoder.encode(JSON.stringify(payload)))
+      .setProtectedHeader({ kid: this.jwk.kid })
+      .encrypt(key);
+    return { payload: encodedPayload };
+  }
+}
+
+export default PingEncryptionPlugin;
