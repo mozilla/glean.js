82b8c296c843848ba838a23fff1d9e23004901df
brizental <brizental@gmail.com>
2021-03-10 10:53:23 +0100

    Add external API fordebug view and source tags

--- glean/src/index/qt.ts
+++ glean/src/index/qt.ts
@@ -74,6 +74,55 @@ export default {
     Glean.setLogPings(flag);
   },
 
+  /**
+   * Sets the `debugViewTag` debug option.
+   *
+   * When this property is set, all subsequent outgoing pings will include the `X-Debug-Id` header
+   * which will redirect them to the ["Ping Debug Viewer"](https://debug-ping-preview.firebaseapp.com/).
+   *
+   * To unset the `debugViewTag` call `Glean.unsetDebugViewTag();
+   *
+   * @param value The value of the header.
+   *        This value must satify the regex `^[a-zA-Z0-9-]{1,20}$` otherwise it will be ignored.
+   */
+  setDebugViewTag(value: string): void {
+    Glean.setDebugViewTag(value);
+  },
+
+  /**
+   * Unsets the `debugViewTag` debug option.
+   *
+   * This is a no-op is case there is no `debugViewTag` set at the moment.
+   */
+  unsetDebugViewTag(): void {
+    Glean.unsetDebugViewTag();
+  },
+
+  /**
+   * Sets the `sourceTags` debug option.
+   *
+   * Ping tags will show in the destination datasets, after ingestion.
+   *
+   * **Note** Setting `sourceTags` will override all previously set tags.
+   *
+   * To unset the `sourceTags` call `Glean.unsetSourceTags();
+   *
+   * @param value A vector of at most 5 valid HTTP header values.
+   *        Individual tags must match the regex: "[a-zA-Z0-9-]{1,20}".
+   */
+  setSourceTags(value: string[]): void {
+    Glean.setSourceTags(value);
+  },
+
+  /**
+   * Unsets the `sourceTags` debug option.
+   *
+   * This is a no-op is case there are no `sourceTags` set at the moment.
+   */
+  unsetSourceTags(): void {
+    Glean.unsetSourceTags();
+  },
+
   _private: {
     PingType,
     BooleanMetricType,
--- glean/src/index/webext.ts
+++ glean/src/index/webext.ts
@@ -54,7 +54,7 @@ export default {
   },
 
   /**
-   * Sets the `logPings` flag.
+   * Sets the `logPings` debug option.
    *
    * When this flag is `true` pings will be logged
    * to the console right before they are collected.
@@ -63,5 +63,54 @@ export default {
    */
   setLogPings(flag: boolean): void {
     Glean.setLogPings(flag);
-  }
+  },
+
+  /**
+   * Sets the `debugViewTag` debug option.
+   *
+   * When this property is set, all subsequent outgoing pings will include the `X-Debug-Id` header
+   * which will redirect them to the ["Ping Debug Viewer"](https://debug-ping-preview.firebaseapp.com/).
+   *
+   * To unset the `debugViewTag` call `Glean.unsetDebugViewTag();
+   *
+   * @param value The value of the header.
+   *        This value must satify the regex `^[a-zA-Z0-9-]{1,20}$` otherwise it will be ignored.
+   */
+  setDebugViewTag(value: string): void {
+    Glean.setDebugViewTag(value);
+  },
+
+  /**
+   * Unsets the `debugViewTag` debug option.
+   *
+   * This is a no-op is case there is no `debugViewTag` set at the moment.
+   */
+  unsetDebugViewTag(): void {
+    Glean.unsetDebugViewTag();
+  },
+
+  /**
+   * Sets the `sourceTags` debug option.
+   *
+   * Ping tags will show in the destination datasets, after ingestion.
+   *
+   * **Note** Setting `sourceTags` will override all previously set tags.
+   *
+   * To unset the `sourceTags` call `Glean.unsetSourceTags();
+   *
+   * @param value A vector of at most 5 valid HTTP header values.
+   *        Individual tags must match the regex: "[a-zA-Z0-9-]{1,20}".
+   */
+  setSourceTags(value: string[]): void {
+    Glean.setSourceTags(value);
+  },
+
+  /**
+   * Unsets the `sourceTags` debug option.
+   *
+   * This is a no-op is case there are no `sourceTags` set at the moment.
+   */
+  unsetSourceTags(): void {
+    Glean.unsetSourceTags();
+  },
 };
