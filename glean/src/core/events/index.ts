8c7acbee90228636c4b36602fd18713f104d48b9
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-04-11 20:49:27 +0200

    Use imported types in the encryption plugin

--- glean/src/core/events/index.ts
+++ glean/src/core/events/index.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Plugin from "../../plugins/index.js";
+import type Plugin from "../../plugins/index.js";
 
 import { PingPayload } from "../pings/ping_payload.js";
 import { JSONObject } from "../utils.js";
