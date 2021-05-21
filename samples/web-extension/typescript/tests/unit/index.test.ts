cca9be449adb6c3b5d355ce6db7a26d959bc88a1
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-21 02:26:06 -0700

    Added a CI job for the Typescript web-extension example.

--- samples/web-extension/typescript/tests/unit/index.test.ts
+++ samples/web-extension/typescript/tests/unit/index.test.ts
@@ -2,20 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { strict as assert } from 'assert';
+import { strict as assert } from "assert";
 import Glean from "@mozilla/glean/webext";
 
 import * as sample from "../../src/generated/sample.js";
 
-describe('webext', () => {
+describe("webext", () => {
     const testAppId = `webext.js.test.${this.title}`;
 
     beforeEach(async () => {
         await Glean.testResetGlean(testAppId);
     });
     
-    describe('sample test', () => {
-        it('a metric recording works', async () => {
+    describe("sample test", () => {
+        it("a metric recording works", async () => {
             sample.popupOpened.add(1);
             assert.equal(await sample.popupOpened.testGetValue(), 1);
         });
