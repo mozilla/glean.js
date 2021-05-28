094be071d054e3e2aec0d9eaa0990f07d4e2ecfd
brizental <beatriz.rizental@gmail.com>
2021-05-28 17:08:57 +0200

    Build the Qt library as a QML module

---
+++ bin/prepare-qml-module.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -eo pipefail
+
+WORKSPACE_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )"
+
+# Get @mozilla/glean current version without the patch version.
+# Qt import paths may only have major and minor version.
+GLEAN_VERSION=$(node -p -e "require('${WORKSPACE_ROOT}/glean/package.json').version.split('.').reverse().slice(1).reverse().join('.')")
+
+# Create the qmldir file
+FILE=glean/dist/qt/org/mozilla/Glean/qmldir
+touch "${WORKSPACE_ROOT}/${FILE}"
+{
+    echo module org.mozilla.Glean
+    echo Glean $GLEAN_VERSION glean.js
+} >> "${WORKSPACE_ROOT}/${FILE}"
+
+# Add the glean.js file to the final module
+cp "${WORKSPACE_ROOT}/glean/qt.entry.js" "${WORKSPACE_ROOT}/glean/dist/qt/org/mozilla/Glean/glean.js"
