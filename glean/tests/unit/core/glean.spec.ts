9d03b1cf7124deff312847cc451a577749dc6a48
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-28 18:52:59 -0700

    Removed tests for the unset API.

--- glean/tests/unit/core/glean.spec.ts
+++ glean/tests/unit/core/glean.spec.ts
@@ -376,18 +376,6 @@ describe("Glean", function() {
     assert.ok(dispatchSpy.notCalled);
   });
 
-  it("unsetting a debug view tag works", async function () {
-    // Unsetting when nothing is set is a no-op
-    Glean.unsetDebugViewTag();
-    assert.strictEqual(Glean.debugViewTag, undefined);
-
-    Glean.setDebugViewTag("test");
-    Glean.unsetDebugViewTag();
-
-    await Context.dispatcher.testBlockOnQueue();
-    assert.strictEqual(Glean.debugViewTag, undefined);
-  });
-
   it("setting source tags on initialize works", async function () {
     await Glean.testUninitialize();
     await Glean.testInitialize(testAppId, true, { debug: { sourceTags: ["1", "2", "3", "4", "5"] } });
