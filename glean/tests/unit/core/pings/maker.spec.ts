3e0b82f14cb825adc3c228a2ef297c7697c240f2
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-06-03 20:05:30 -0700

    Removed the testResetGlean from maker.spec.ts.

--- glean/tests/unit/core/pings/maker.spec.ts
+++ glean/tests/unit/core/pings/maker.spec.ts
@@ -134,7 +134,6 @@ describe("PingMaker", function() {
       "X-Source-Tags": "tag1,tag2,tag3"
     }, PingMaker.getPingHeaders(Context.debugOptions));
 
-    Glean.testResetGlean();
     await Context.dispatcher.testBlockOnQueue();
     assert.strictEqual(PingMaker.getPingHeaders(Context.debugOptions), undefined);
   });
