33adb9c35b8a2edb13806f19f369eb9776deb6ed
brizental <beatriz.rizental@gmail.com>
2021-06-25 16:08:34 +0200

    Use ErrorType from JS sample tests

--- samples/web-extension/javascript/tests/unit/index.test.js
+++ samples/web-extension/javascript/tests/unit/index.test.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { strict as assert } from 'assert';
-import Glean from "@mozilla/glean/webext";
+import { strict as assert } from "assert";
+import Glean, { ErrorType } from "@mozilla/glean/webext";
 
 import * as sample from "../../src/generated/sample.js";
 
@@ -18,6 +18,10 @@ describe('webext', function () {
     it('a metric recording works', async function () {
       sample.popupOpened.add(1);
       assert.equal(await sample.popupOpened.testGetValue(), 1);
+      assert.equal(
+        await sample.popupOpened.testGetNumRecordedErrors(ErrorType.InvalidValue),
+        0
+      );
     });
   });
 });
