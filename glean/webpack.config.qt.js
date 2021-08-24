58fffebe5c129ce7713d54f62ab687ebba28c422
brizental <beatriz.rizental@gmail.com>
2021-08-24 16:48:45 +0200

    Build unminified version for Qt as well

--- glean/webpack.config.qt.js
+++ glean/webpack.config.qt.js
@@ -9,8 +9,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const OUTPUT = "glean.lib.js";
-
 /**
  * Hacky plugin that removes ".js" extensions from imports before resolving.
  *
@@ -40,51 +38,77 @@ class TsResolvePlugin {
  * and not anywhere else.
  */
 class RemoveUseStrictPlugin {
+  constructor(output) {
+    this.output = output;
+  }
+
   apply(compiler) {
     compiler.hooks.shouldEmit.tap("RemoveUseStrictPlugin", compilation => {
-      compilation.assets[OUTPUT]._value = compilation.assets[OUTPUT]._value.replace("\"use strict\";", "");
+      if (compilation.assets[this.output]._children) {
+        compilation.assets[this.output]._children[0]._value = compilation.assets[this.output]._children[0]._value.replace("\"use strict\";", "");
+      } else {
+        compilation.assets[this.output]._value = compilation.assets[this.output]._value?.replace("\"use strict\";", "");
+      }
       return true;
     });
   }
 }
 
-export default {
-  entry: "./src/index/qt.ts",
+// eslint-disable-next-line
+function getBaseConfig(output) {
+  return {
+    entry: "./src/index/qt.ts",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+          options: {
+            onlyCompileBundledFiles: true,
+            // This path is resolved relative to the entry file, ./src/index/qt.ts
+            // See: https://github.com/TypeStrong/ts-loader#configfile
+            configFile: "../../tsconfig/qt.json"
+          },
+        },
+      ],
+    },
+    resolve: {
+      modules: ["node_modules"],
+      extensions: [ ".tsx", ".ts", ".js" ],
+      plugins: [
+        new TsResolvePlugin()
+      ]
+    },
+    plugins: [
+      new RemoveUseStrictPlugin(output),
+    ],
+    output: {
+      path: path.resolve(__dirname, "dist/qt/org/mozilla/Glean"),
+      filename: output,
+      libraryTarget: "var",
+      library: "Glean",
+    }
+  };
+}
+
+const productionConfig = {
+  ...getBaseConfig("glean.lib.js"),
   mode: "production",
   optimization: {
     usedExports: true,
     providedExports: true,
     sideEffects: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-        options: {
-          onlyCompileBundledFiles: true,
-          // This path is resolved relative to the entry file, ./src/index/qt.ts
-          // See: https://github.com/TypeStrong/ts-loader#configfile
-          configFile: "../../tsconfig/qt.json"
-        },
-      },
-    ],
-  },
-  resolve: {
-    modules: ["node_modules"],
-    extensions: [ ".tsx", ".ts", ".js" ],
-    plugins: [
-      new TsResolvePlugin()
-    ]
-  },
-  plugins: [
-    new RemoveUseStrictPlugin(),
-  ],
-  output: {
-    path: path.resolve(__dirname, "dist/qt/org/mozilla/Glean"),
-    filename: OUTPUT,
-    libraryTarget: "var",
-    library: "Glean",
-  }
 };
+
+const developmentConfig = {
+  ...getBaseConfig("glean.dev.js"),
+  mode: "development",
+  devtool: false
+};
+
+export default [
+  productionConfig,
+  developmentConfig
+];
