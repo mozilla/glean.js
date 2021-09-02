e953c8cc225323f3559db97851181726ff9bd67e
brizental <beatriz.rizental@gmail.com>
2021-09-02 19:05:34 +0200

    Create Node.js sample CLI app placeholder

---
+++ samples/node/index.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+import Glean from "@mozilla/glean/node";
+
+Glean.initialize("glean-sample-cli", true);
+console.log("Hey! Glean initialize succesfully!");
