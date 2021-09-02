3262b1785f1109ac5f06c9d796beb4be3f35b0de
brizental <beatriz.rizental@gmail.com>
2021-09-02 19:03:55 +0200

    Implement placeholder Node.js platform module

---
+++ glean/src/platform/node/index.ts
@@ -0,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Placeholding with the Test Platform for now
+// until we implement each component of the Node platform.
+export { default } from "../test/index.js";
