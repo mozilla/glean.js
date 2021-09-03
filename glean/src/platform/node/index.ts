49da6f20c2e0dd56798df2bf0cd16c56cfcb0e6f
brizental <beatriz.rizental@gmail.com>
2021-09-03 14:19:20 +0200

    Attend to review comments

--- glean/src/platform/node/index.ts
+++ glean/src/platform/node/index.ts
@@ -7,11 +7,13 @@ import type Platform from "../index.js";
 
 // We will still use the TestPlatform as a placeholder
 // for the other Node.js modules that have not been implemented yet.
+// See Bug 1728810 (Uploader) and Bug 1728807 (Storage).
 import TestPlatform from "../test/index.js";
 
 const NodePlatform: Platform = {
   ...TestPlatform,
   info: PlatformInfo,
+  name: "node"
 };
 
 export default NodePlatform;
