86096805bee98b7532ca7f17ede3950dfca52745
brizental <beatriz.rizental@gmail.com>
2021-10-12 12:42:01 +0200

    Accept arch and os version as init params on Qt

--- glean/src/core/config.ts
+++ glean/src/core/config.ts
@@ -29,6 +29,16 @@ export interface ConfigurationInterface {
   plugins?: Plugin[],
   // The HTTP client implementation to use for uploading pings.
   httpClient?: Uploader,
+  // Qt-only fields
+  //
+  // These values are not easily accessible from QML,
+  // so we expose them as init fields for the caller to fill them out.
+  //
+  // The architecture of the device (e.g. "arm", "x86").
+  readonly architecture?: string,
+  // The user-visible version of the operating system (e.g. "1.2.3").
+  // If the version detection fails, this metric gets set to Unknown.
+  readonly osVersion?: string,
 }
 
 // Important: the `Configuration` should only be used internally by the Glean singleton.
@@ -41,6 +51,12 @@ export class Configuration implements ConfigurationInterface {
   readonly appDisplayVersion?: string;
   // The server pings are sent to.
   readonly serverEndpoint: string;
+  // The architecture of the device (e.g. "arm", "x86").
+  readonly architecture?: string;
+  // The user-visible version of the operating system (e.g. "1.2.3").
+  // If the version detection fails, this metric gets set to Unknown.
+  readonly osVersion?: string;
+
   // Debug configuration.
   debug: DebugOptions;
   // The HTTP client implementation to use for uploading pings.
@@ -50,6 +66,8 @@ export class Configuration implements ConfigurationInterface {
     this.channel = config?.channel;
     this.appBuild = config?.appBuild;
     this.appDisplayVersion = config?.appDisplayVersion;
+    this.architecture = config?.architecture;
+    this.osVersion = config?.osVersion;
 
     this.debug = Configuration.sanitizeDebugOptions(config?.debug);
 
