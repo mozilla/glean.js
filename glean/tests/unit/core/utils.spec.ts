f5e05e97695ddc35b029cc30b7f1b02e4895a406
brizental <beatriz.rizental@gmail.com>
2021-10-18 16:06:03 +0200

    Fix utils test and update package-lock

--- glean/tests/unit/core/utils.spec.ts
+++ glean/tests/unit/core/utils.spec.ts
@@ -99,7 +99,8 @@ describe("utils", function() {
     assert.strictEqual(utils.isInteger(-100000), true);
     assert.strictEqual(utils.isInteger(999999999999999), true);
     assert.strictEqual(utils.isInteger(5.0), true);
-    assert.strictEqual(utils.isInteger(5.000000000000001), true);
+    // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+    assert.strictEqual(utils.isInteger(5.0000000000000001), true);
   });
 
   it("sanitizeApplicationId works correctly", function() {
