c397cd8e54c70479d2296d8932d0e9aebbb01042
brizental <beatriz.rizental@gmail.com>
2021-10-19 16:19:14 +0200

    Add Node.js and Web builds to benchmarks size report
    There is a few things going on here, which I was not able to break down
    into multiple commits in time.
    
    1. I remove the platform specific scripts and webpack config in favor of
       a single one which change target platform based on the PLATFORM
       environment variable.
    2. I move the build and link scripts into the run.sh script and add
       logic to create builds for also web and node.js in there.
    3. I change report.js to also include web and node.js into the report.`

--- benchmarks/size/min.js
+++ benchmarks/size/min.js
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Glean from "@mozilla/glean/webext";
+// GLEAN will be conditoned on the PLATFORM environment variable.
+// Check `./webpack.config.js` for more info.
+import Glean from "GLEAN";
 
 // We import everything and log it, so that we are sure all imports are used
 // and webpack is required to actually include them in the final bundle.
