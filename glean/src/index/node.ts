343e8c11868e6af4c96ddb70b8e14c3e51599a34
brizental <beatriz.rizental@gmail.com>
2021-11-15 09:37:09 +0100

    Expose Uploader, UploadResult and UploadResultStatus
    Left Qt out of this on purpose, seems to be a bit more involved to
    expose these over there and no one is currently asking for it.

--- glean/src/index/node.ts
+++ glean/src/index/node.ts
@@ -6,4 +6,5 @@ import platform from "../platform/node/index.js";
 import base from "./base.js";
 
 export { ErrorType } from "../core/error/error_type.js";
+export { default as Uploader, UploadResult, UploadResultStatus } from "../core/upload/uploader.js";
 export default base(platform);
