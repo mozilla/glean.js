e467a98ca73cfefa049204fbdf6bb1bb1bf3f834
brizental <beatriz.rizental@gmail.com>
2021-11-24 11:59:36 +0100

    Implement a PingUploadWorker class
    The worker will be responsible for assembling ping requests
    and performing the actual ping uploads.

---
+++ glean/src/core/upload/worker.ts
@@ -0,0 +1,179 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { gzipSync, strToU8 } from "fflate";
+
+import type { QueuedPing } from "./manager";
+import type Uploader from "./uploader.js";
+import type { UploadTask} from "./task.js";
+import { GLEAN_VERSION } from "../constants.js";
+import { Context } from "../context.js";
+import log, { LoggingLevel } from "../log.js";
+import Policy from "./policy.js";
+import { UploadResult, UploadResultStatus } from "./uploader.js";
+import { UploadTaskTypes } from "./task.js";
+
+const LOG_TAG = "core.Upload.PingUploadWorker";
+
+// Error to be thrown in case the final ping body is larger than MAX_PING_BODY_SIZE.
+class PingBodyOverflowError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "PingBodyOverflow";
+  }
+}
+
+class PingUploadWorker {
+  private currentJob?: Promise<void>;
+
+  constructor (
+    private readonly uploader: Uploader,
+    private readonly serverEndpoint: string,
+    private readonly policy = new Policy()
+  ) {}
+
+  /**
+   * Builds a ping request.
+   *
+   * This includes:
+   *
+   * 1. Includes Glean required headers to the ping;
+   *    These are the headers described in https://mozilla.github.io/glean/book/user/pings/index.html?highlight=headers#submitted-headers
+   * 2. Stringifies the body.
+   *
+   * @param ping The ping to include the headers in.
+   * @returns The updated ping.
+   */
+  private async buildPingRequest(ping: QueuedPing): Promise<{
+    headers: Record<string, string>,
+    payload: string | Uint8Array
+  }> {
+    let headers = ping.headers || {};
+    headers = {
+      ...ping.headers,
+      "Content-Type": "application/json; charset=utf-8",
+      "Date": (new Date()).toISOString(),
+      "X-Client-Type": "Glean.js",
+      "X-Client-Version": GLEAN_VERSION,
+      "X-Telemetry-Agent": `Glean/${GLEAN_VERSION} (JS on ${await Context.platform.info.os()})`
+    };
+
+    const stringifiedBody = JSON.stringify(ping.payload);
+    // We prefer using `strToU8` instead of TextEncoder directly,
+    // because it will polyfill TextEncoder if it's not present in the environment.
+    // Environments that don't provide TextEncoder are IE and most importantly QML.
+    const encodedBody = strToU8(stringifiedBody);
+
+    let finalBody: string | Uint8Array;
+    let bodySizeInBytes: number;
+    try {
+      finalBody = gzipSync(encodedBody);
+      bodySizeInBytes = finalBody.length;
+      headers["Content-Encoding"] = "gzip";
+    } catch {
+      finalBody = stringifiedBody;
+      bodySizeInBytes = encodedBody.length;
+    }
+
+    if (bodySizeInBytes > this.policy.maxPingBodySize) {
+      throw new PingBodyOverflowError(
+        `Body for ping ${ping.identifier} exceeds ${this.policy.maxPingBodySize}bytes. Discarding.`
+      );
+    }
+
+    headers["Content-Length"] = bodySizeInBytes.toString();
+    return {
+      headers,
+      payload: finalBody
+    };
+  }
+
+  /**
+   * Attempts to upload a ping.
+   *
+   * @param ping The ping object containing headers and payload.
+   * @returns The status number of the response or `undefined` if unable to attempt upload.
+   */
+  private async attemptPingUpload(ping: QueuedPing): Promise<UploadResult> {
+    try {
+      const finalPing = await this.buildPingRequest(ping);
+      return await this.uploader.post(
+        `${this.serverEndpoint}${ping.path}`,
+        finalPing.payload,
+        finalPing.headers
+      );
+    } catch(e) {
+      log(LOG_TAG, [ "Error trying to build ping request:", e ], LoggingLevel.Warn);
+      // An unrecoverable failure will make sure the offending ping is removed from the queue and
+      // deleted from the database, which is what we want here.
+      return new UploadResult(UploadResultStatus.UnrecoverableFailure);
+    }
+  }
+
+  /**
+   * Start a loop to get queued pings and attempt upload.
+   *
+   * @param getUploadTask A function that returns an UploadTask.
+   * @param processUploadResponse A function that processes an UploadResponse.
+   * @returns A promise whihc resolves on a Done_UploadTask is received.
+   */
+  private async workInternal(
+    getUploadTask: () => UploadTask,
+    processUploadResponse: (ping: QueuedPing, result: UploadResult) => Promise<void>,
+  ): Promise<void> {
+    while(true) {
+      const nextTask = getUploadTask();
+      switch (nextTask.type) {
+      case UploadTaskTypes.Upload:
+        const result = await this.attemptPingUpload(nextTask.ping);
+        await processUploadResponse(nextTask.ping, result);
+        continue;
+      // TODO(bug1727076): Actually set a timer to continue once timeout is complete.
+      // Note that the timer must be cleared in case an abort signal is issued.
+      case UploadTaskTypes.Wait:
+      case UploadTaskTypes.Done:
+        this.currentJob = undefined;
+        return;
+      }
+    }
+  }
+
+  /**
+   * Kick start non-blocking asynchronous internal loop to get and act on upload tasks.
+   *
+   * If a job is currently ongoing, this is a no-op.
+   *
+   * @param getUploadTask A function that returns an UploadTask.
+   * @param processUploadResponse A function that processes an UploadResponse.
+   */
+  work(
+    getUploadTask: () => UploadTask,
+    processUploadResponse: (ping: QueuedPing, result: UploadResult) => Promise<void>,
+  ): void {
+    if (!this.currentJob) {
+      this.currentJob = this.workInternal(getUploadTask, processUploadResponse);
+    }
+  }
+
+  /**
+   * Allows to wait for current job completion.
+   *
+   * # Warning
+   *
+   * Use only at times when you know it is not possible for this to hang too long
+   * i.e. at times when you know how many pings are enqueued.
+   *
+   * @returns A promise which resolves once the current ongoing job is complete.
+   *          If there is no ongoing job, the returned promise will resolve immediately.
+   */
+  async blockOnCurrentJob() {
+    if (this.currentJob) {
+      return this.currentJob;
+    }
+
+    return Promise.resolve();
+  }
+}
+
+export default PingUploadWorker;
---
+++ glean/tests/unit/core/upload/worker.spec.ts
@@ -0,0 +1,276 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import assert from "assert";
+import sinon from "sinon";
+
+import { UploadTaskTypes } from "../../../../src/core/upload/task";
+import type { UploadTask } from "../../../../src/core/upload/task";
+import uploadTaskFactory from "../../../../src/core/upload/task";
+import PingUploadWorker from "../../../../src/core/upload/worker";
+import TestPlatform from "../../../../src/platform/test";
+import Glean from "../../../../src/core/glean";
+import { CounterUploader } from "../../../utils";
+import { makePath } from "../../../../src/core/pings/maker";
+import { Context } from "../../../../src/core/context";
+import Policy from "../../../../src/core/upload/policy";
+import { UploadResultStatus } from "../../../../src/core/upload/uploader";
+import type { UploadResult } from "../../../../src/core/upload/uploader";
+
+const sandbox = sinon.createSandbox();
+
+const MOCK_PING_NAME = "ping";
+const MOCK_PING_IDENTIFIER = "identifier";
+
+/**
+ * Build mock upload task of a given type.
+ *
+ * @param type The type of the task to build.
+ * @returns An upload task.
+ */
+function buildMockTask(type: UploadTaskTypes): UploadTask {
+  switch(type) {
+  case UploadTaskTypes.Done:
+    return uploadTaskFactory.done();
+  case UploadTaskTypes.Wait:
+    return uploadTaskFactory.wait(10);
+  case UploadTaskTypes.Upload:
+    return uploadTaskFactory.upload({
+      identifier: MOCK_PING_IDENTIFIER,
+      collectionDate: "2021-11-23T17:23:31.661Z",
+      payload: {
+        ping_info: {
+          seq: 1,
+          start_time: "2020-01-11+01:00",
+          end_time: "2020-01-12+01:00",
+        },
+        client_info: {
+          telemetry_sdk_build: "32.0.0"
+        }
+      },
+      path: makePath(
+        MOCK_PING_IDENTIFIER,
+        { name: MOCK_PING_NAME, includeClientId: true, sendIfEmpty: true }
+      )
+    });
+  }
+}
+
+/**
+ * Generator that yields mock tasks of given types in given order.
+ *
+ * @param tasks The tasks to yield.
+ * @yields A task.
+ * @returns The last task is not yielded, it's returned.
+ */
+function* mockGetUploadTasks(tasks: UploadTaskTypes[]): Generator<UploadTask, UploadTask> {
+  let index = 0;
+  while(index < tasks.length - 1) {
+    yield buildMockTask(tasks[index]);
+    index++;
+  }
+
+  return buildMockTask(tasks[index]);
+}
+
+describe("PingUploadWorker", function() {
+  const testAppId = `gleanjs.test.${this.title}`;
+
+  before(async function () {
+    // We call this only once so that the platform is set
+    // and we are able to access the Platform info.
+    await Glean.testResetGlean(testAppId, true);
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it("whenever a Done task is received the currentJob is ended", async function() {
+    const worker = new PingUploadWorker(TestPlatform.uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([ UploadTaskTypes.Done, UploadTaskTypes.Upload ]);
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+
+    await worker.blockOnCurrentJob();
+    // Check that we never got to the Upload task enqueued,
+    // due to bailing out when we found a Done task.
+    assert.strictEqual(tasksGenerator.next().value.type, UploadTaskTypes.Upload);
+  });
+
+  it("whenever a Wait task is received the currentJob is ended", async function() {
+    const worker = new PingUploadWorker(TestPlatform.uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([ UploadTaskTypes.Wait, UploadTaskTypes.Upload ]);
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+
+    await worker.blockOnCurrentJob();
+    // Check that we never got to the Upload task enqueued,
+    // due to bailing out when we found a Wait task.
+    assert.strictEqual(tasksGenerator.next().value.type, UploadTaskTypes.Upload);
+  });
+
+  it("whenever an Upload task is received upload attempts are made", async function() {
+    const uploader = new CounterUploader();
+    const worker = new PingUploadWorker(uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([
+      ...Array(10).fill(UploadTaskTypes.Upload) as UploadTaskTypes.Upload[],
+      // Always end with a Done task to make sure the worker stops asking for more tasks.
+      UploadTaskTypes.Done
+    ]);
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+
+    await worker.blockOnCurrentJob();
+    // All upload requests were completed.
+    assert.strictEqual(uploader.count, 10);
+  });
+
+  it("ping requests are built correctly", async function () {
+    const postSpy = sandbox.spy(TestPlatform.uploader, "post");
+
+    const worker = new PingUploadWorker(TestPlatform.uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([
+      UploadTaskTypes.Upload,
+      // Always end with a Done task to make sure the worker stops asking for more tasks.
+      UploadTaskTypes.Done
+    ]);
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+    await worker.blockOnCurrentJob();
+
+    const url = postSpy.firstCall.args[0].split("/");
+    const appId = url[url.length - 4];
+    const documentId = url[url.length - 1];
+    const headers = postSpy.firstCall.args[2] || {};
+
+    assert.strictEqual(documentId, MOCK_PING_IDENTIFIER);
+    assert.strictEqual(appId, Context.applicationId);
+
+    assert.ok("Date" in headers);
+    assert.ok("Content-Length" in headers);
+    assert.ok("Content-Type" in headers);
+    assert.ok("X-Client-Type" in headers);
+    assert.ok("X-Client-Version" in headers);
+    assert.ok("X-Telemetry-Agent" in headers);
+    assert.strictEqual(headers["Content-Encoding"], "gzip");
+  });
+
+  it("succesfull upload attemps return the correct upload result", async function () {
+    const postSpy = sandbox.stub(TestPlatform.uploader, "post")
+      .onFirstCall().callsFake(() => Promise.resolve({
+        status: 200,
+        result: UploadResultStatus.Success
+      }))
+      .onSecondCall().callsFake(() => Promise.resolve({
+        status: 404,
+        result: UploadResultStatus.Success
+      }))
+      .onThirdCall().callsFake(() => Promise.resolve({
+        status: 500,
+        result: UploadResultStatus.Success
+      }));
+
+    const worker = new PingUploadWorker(TestPlatform.uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([
+      UploadTaskTypes.Upload,
+      UploadTaskTypes.Upload,
+      UploadTaskTypes.Upload,
+      // Always end with a Done task to make sure the worker stops asking for more tasks.
+      UploadTaskTypes.Done
+    ]);
+
+    const processSpy = sandbox.spy();
+    worker.work(
+      () => tasksGenerator.next().value,
+      processSpy
+    );
+    await worker.blockOnCurrentJob();
+
+    assert.strictEqual(postSpy.callCount, 3);
+
+    const resultSuccess = (processSpy.firstCall.args as [never, UploadResult])[1];
+    assert.strictEqual(resultSuccess.result, UploadResultStatus.Success);
+    assert.strictEqual(resultSuccess.status, 200);
+
+    const resultNotFound = (processSpy.secondCall.args as [never, UploadResult])[1];
+    assert.strictEqual(resultNotFound.result, UploadResultStatus.Success);
+    assert.strictEqual(resultNotFound.status, 404);
+
+    const resultInternalServerError = (processSpy.thirdCall.args as [never, UploadResult])[1];
+    assert.strictEqual(resultInternalServerError.result, UploadResultStatus.Success);
+    assert.strictEqual(resultInternalServerError.status, 500);
+  });
+
+  it("pings which exceed max ping body size are not sent and an correct result is returned", async function () {
+    const uploader = new CounterUploader();
+    // Create a new worker with a very low max ping body size,
+    // so that virtually any ping body will throw an error.
+    const worker = new PingUploadWorker(
+      uploader,
+      "https://my-glean-test.com",
+      new Policy(
+        3, // maxWaitAttempts
+        3, // maxRecoverableFailures
+        1, // maxPingBodySize
+      )
+    );
+    const tasksGenerator = mockGetUploadTasks([
+      UploadTaskTypes.Upload,
+      // Always end with a Done task to make sure the worker stops asking for more tasks.
+      UploadTaskTypes.Done
+    ]);
+
+    const processSpy = sandbox.spy();
+    worker.work(
+      () => tasksGenerator.next().value,
+      processSpy
+    );
+    await worker.blockOnCurrentJob();
+
+    // Check that none of those pings were actually sent.
+    assert.strictEqual(uploader.count, 0);
+    // Check that an UnrecoverableFailure was returned by the upload attempt.
+    const uploadResult = (processSpy.firstCall.args as [never, UploadResult])[1];
+    assert.strictEqual(uploadResult.result, UploadResultStatus.UnrecoverableFailure);
+  });
+
+  it("attempting to start a new job when another is ongoing is a no-op", async function() {
+    const worker = new PingUploadWorker(TestPlatform.uploader, "https://my-glean-test.com");
+    const tasksGenerator = mockGetUploadTasks([
+      ...Array(10).fill(UploadTaskTypes.Upload) as UploadTaskTypes.Upload[],
+      // Always end with a Done task to make sure the worker stops asking for more tasks.
+      UploadTaskTypes.Done
+    ]);
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+
+    const initialCurrentJob = worker["currentJob"];
+
+    worker.work(
+      () => tasksGenerator.next().value,
+      () => Promise.resolve()
+    );
+
+    assert.strictEqual(worker["currentJob"], initialCurrentJob);
+
+    // Wait for the queue to be processed before exiting the test.
+    await worker.blockOnCurrentJob();
+  });
+});
