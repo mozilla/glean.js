495f7dc0057c42b72e5639fa3e7ac4df4f6acf3a
brizental <beatriz.rizental@gmail.com>
2022-01-20 11:37:05 +0100

    Refactor benchmarks reporting code to JS only

--- benchmarks/size/report.js
+++ benchmarks/size/report.js
@@ -2,128 +2,107 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { statSync  } from "fs";
-
 import { request } from "@octokit/request";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import {
+  METRIC_TYPES,
+  PLUGINS,
+  getCustomLibSize,
+  formatBytes,
+  executeCmd,
+  getQtBundleSize
+} from "./utils.js";
 
-// @mozilla/glean/webext
-import webextStats from "../dist/size/webext/report.json";
-import mainWebextStats from "../dist/size/webext/main/report.json";
-// @mozilla/glean/web
-import webStats from "../dist/size/web/report.json";
-import mainWebStats from "../dist/size/web/main/report.json";
-// @mozilla/glean/node
-import nodeStats from "../dist/size/node/report.json";
-import mainNodeStats from "../dist/size/node/main/report.json";
+async function buildStatsMap() {
 
-// The Qt bundle size is special, because the Qt build is not part of the @mozilla/glean npm package.
-// It is already bundled and distributed with each new release on the Github releases page.
-// We build it over on the `glean/` folder and copy it with specific names to this folder for convenience.
-//
-// In Qt the bundle always contains all metric types and plugins, so there is no min/max.
-const qtBundleStats = statSync(path.resolve(__dirname, "../dist/size/qt/glean.lib.js"));
-const mainQtBundleStats = statSync(path.resolve(__dirname, "../dist/size/qt/main/glean.lib.js"));
-
-// Function shamelessly copied from
-// https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
-const UNITS = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-function formatBytes(bytes) {
-  let l = 0, n = parseInt(bytes, 10) || 0;
-  while(n >= 1024 && ++l){
-    n = n/1024;
-  }
-  return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + UNITS[l]);
+  const qtBundleStats = getQtBundleSize();
+  return [
+    {
+      name: "Web Extension",
+      max: await getCustomLibSize("webext", METRIC_TYPES, PLUGINS),
+      min: await getCustomLibSize("webext")
+    },
+    {
+      name: "Website",
+      max: await getCustomLibSize("web", METRIC_TYPES, PLUGINS),
+      min: await getCustomLibSize("web")
+    },
+    {
+      name: "Node.js",
+      max: await getCustomLibSize("node", METRIC_TYPES, PLUGINS),
+      min: await getCustomLibSize("node")
+    },
+    {
+      name: "Qt/QML",
+      max: qtBundleStats,
+      min: qtBundleStats
+    }
+  ];
 }
 
-const stats = [
-  {
-    name: "Web Extension",
-    max: webextStats[0].parsedSize,
-    min: webextStats[1].parsedSize,
-    main: {
-      max: mainWebextStats[0].parsedSize,
-      min: mainWebextStats[1].parsedSize,
-    }
-  },
-  {
-    name: "Website",
-    max: webStats[0].parsedSize,
-    min: webStats[1].parsedSize,
-    main: {
-      max: mainWebStats[0].parsedSize,
-      min: mainWebStats[1].parsedSize,
-    }
-  },
-  {
-    name: "Node.js",
-    max: nodeStats[0].parsedSize,
-    min: nodeStats[1].parsedSize,
-    main: {
-      max: mainNodeStats[0].parsedSize,
-      min: mainNodeStats[1].parsedSize,
-    }
-  },
-  {
-    name: "Qt/QML",
-    max: qtBundleStats.size,
-    min: qtBundleStats.size,
-    main: {
-      max: mainQtBundleStats.size,
-      min: mainQtBundleStats.size,
-    }
-  }
-];
+try {
+  console.log("Build and link Glean.js from the main branch.");
+  await executeCmd("npm run link:glean:main");
 
-const bulletPoints = stats.map(build => {
-  const increase = build.max - build.main.max;
-  const increasePercentage = parseInt(increase / build.main.max * 100, 10);
-  if (increasePercentage === 0) {
-    return `* **Leave** the size of full **${build.name}** bundle **unchanged**.`
-  } else {
-    const result = increase > 0 ? "Increase" : "Decrease";
-    return `* **${result}** the size of full **${build.name}** bundle build by \`${increasePercentage}%\`.`
-  }
-});
+  const mainStats = await buildStatsMap();
 
-const tableRows = stats.map(build => {
-  const coreIncrease = build.min - build.main.min;
-  const fullIncrease = build.max - build.main.max;
-  return `| **${build.name}** |
-| core only | ${formatBytes(build.main.min)} | ${formatBytes(build.min)} | ${coreIncrease > 0 ? "📈" : "📉"} ${formatBytes(Math.abs(coreIncrease))} |
-| full bundle | ${formatBytes(build.main.max)} | ${formatBytes(build.max)} | ${coreIncrease > 0 ? "📈" : "📉"} ${formatBytes(Math.abs(fullIncrease))} |`
-});
+  console.log("Build and link Glean.js from the current branch.");
+  await executeCmd("npm run link:glean");
+  const newStats = await buildStatsMap();
 
-const report =  `
-# Build size report
-
-Merging ${process.env.CIRCLE_PULL_REQUEST} into [main](https://github.com/mozilla/glean.js) will:
-${bulletPoints.join("\n")}
-
----
-
-| | Current size | New size | Size increase |
-|--:|:---:|:---:|:---:|
-${tableRows.join("\n")}
-`;
-
-if (process.env.DRY_RUN) {
-  console.log(report);
-} else {
-  if (!process.env.GITHUB_TOKEN) {
-    throw new Error("No Github token configured!")
-  }
-  await request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-    owner: process.env.CIRCLE_PROJECT_USERNAME,
-    repo: process.env.CIRCLE_PROJECT_REPONAME,
-    issue_number: process.env.CIRCLE_PR_NUMBER,
-    body: report,
-    headers: {
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
-    },
+  const stats = newStats.map((item, index) => {
+    item.main = mainStats[index];
+    return item;
   });
+
+  const bulletPoints = stats.map(build => {
+    const increase = build.max - build.main.max;
+    const increasePercentage = parseInt(increase / build.main.max * 100, 10);
+    if (increasePercentage === 0) {
+      return `* **Leave** the size of full **${build.name}** bundle **unchanged**.`
+    } else {
+      const result = increase > 0 ? "Increase" : "Decrease";
+      return `* **${result}** the size of full **${build.name}** bundle build by \`${increasePercentage}%\`.`
+    }
+  });
+  
+  const tableRows = stats.map(build => {
+    const coreIncrease = build.min - build.main.min;
+    const fullIncrease = build.max - build.main.max;
+    return `| **${build.name}** |
+  | core only | ${formatBytes(build.main.min)} | ${formatBytes(build.min)} | ${coreIncrease > 0 ? "📈" : "📉"} ${formatBytes(Math.abs(coreIncrease))} |
+  | full bundle | ${formatBytes(build.main.max)} | ${formatBytes(build.max)} | ${coreIncrease > 0 ? "📈" : "📉"} ${formatBytes(Math.abs(fullIncrease))} |`
+  });
+  
+  const report =  `
+  # Build size report
+  
+  Merging ${process.env.CIRCLE_PULL_REQUEST} into [main](https://github.com/mozilla/glean.js) will:
+  ${bulletPoints.join("\n")}
+  
+  ---
+  
+  | | Current size | New size | Size increase |
+  |--:|:---:|:---:|:---:|
+  ${tableRows.join("\n")}
+  `;
+  
+  if (process.env.DRY_RUN) {
+    console.log(report);
+  } else {
+    if (!process.env.GITHUB_TOKEN) {
+      throw new Error("No Github token configured!")
+    }
+    await request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+      owner: process.env.CIRCLE_PROJECT_USERNAME,
+      repo: process.env.CIRCLE_PROJECT_REPONAME,
+      issue_number: process.env.CIRCLE_PR_NUMBER,
+      body: report,
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+  }
+} catch(e) {
+  console.trace(e);
 }
---
+++ benchmarks/size/utils.js
@@ -0,0 +1,158 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { exec } from "child_process";
+
+import webpack from "webpack";
+
+import config from "./webpack.config.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const METRIC_TYPES = [
+  "boolean",
+  "counter",
+  "datetime",
+  "event",
+  "labeled",
+  "quantity",
+  "string",
+  "text",
+  "timespan",
+  "rate",
+  "uuid",
+  "url",
+];
+
+export const PLUGINS = [ "encryption" ];
+
+const UNITS = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+/**
+ * Formats a given number of bytes.
+ *
+ * Copied from:
+ * https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+ *
+ * @param {number} bytes The number of bytes to format.
+ * @returns A formatted representation of the bytes given.
+ */
+export function formatBytes(bytes) {
+  let l = 0, n = parseInt(bytes, 10) || 0;
+  while(n >= 1024 && ++l){
+    n = n/1024;
+  }
+  return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + UNITS[l]);
+}
+
+/**
+ * Executes an arbitrary command.
+ *
+ * @param {string} cmd The command to execute.
+ * @returns A promise which executes once the command is complete. Will resolve even in case of errors.
+ */
+export function executeCmd(cmd) {
+  console.log("Executing command:", cmd);
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, _, stderr) => {
+      if (error) {
+          console.log("Error:", error);
+      }
+      if (stderr) {
+          console.log("Stderr:", stderr);
+      }
+
+      resolve();
+    });
+  });
+}
+
+/**
+ * Builds a custom bundle of Glean.js + an optional array of metric types and plugins.
+ *
+ * Note: This function will use whatever `@mozilla/glean` package is linked at its runtime.
+ *
+ * @param {string} platform The platform to build for, supported platforms are: `web`, `webext` and `node`.
+ *        The Qt build always has the same size because it always includes everything in it,
+ *        so it is not supported here.
+ * @param {[string]} metrics An array of metric types. Supported metrics types are listed in `METRIC_TYPES`.
+ *        If not provided the final bundle won't contain any additional metric types.
+ * @param {[string]} plugins An array of plugins. Supported plugins are listed in `PLUGINS`.
+ *        If not provided the final bundle won't contain any additional plugins.
+ * @returns A number representing the size in bytes of the generated bundle.
+ */
+export async function getCustomLibSize(platform, metrics = [], plugins = []) {
+  console.log(
+    `Creating a custom build for the "${platform}" platform.`,
+    `${metrics.length ? `Importing the ${metrics.join(",")} metric types.` : "Without extra metrics types."}`,
+    `${plugins.length ? `Importing the ${plugins.join(",")} plugins.` : "Without any plugins."}`
+  );
+
+  // Generate custom code importing the given metrics and plugins.
+  const code =
+    `import Glean from "@mozilla/glean/${platform}";
+    ${metrics
+        .map(metric => `import ${metric} from "@mozilla/glean/private/metrics/${metric}";`)
+        .join("\n")}
+    ${plugins
+        .map(plugin => `import ${plugin} from "@mozilla/glean/plugins/${plugin}";`)
+        .join("\n")}
+
+    console.log(JSON.stringify(Glean));
+    ${metrics
+      .map(metric => `console.log(JSON.stringify(${metric}));`)
+      .join("\n")}
+    ${plugins
+      .map(plugin => `console.log(JSON.stringify(${plugin}));`)
+      .join("\n")}
+  `;
+
+  // Create the tmp directory if it doesn't exist.
+  const tmpDir = path.join(__dirname, "tmp");
+  if (!fs.existsSync(tmpDir)){
+    fs.mkdirSync(tmpDir);
+  }
+
+  // Create a file with custom code as content.
+  const outputFilePath = path.join(tmpDir, "glean.lib.js")
+  await new Promise((resolve, reject) => {
+    fs.writeFile(outputFilePath, code, err => {
+      if (err) reject(err);
+
+      resolve();
+    });
+  });
+
+  // Build the library and return the size of the asset generated.
+  const customConfig = config(platform, outputFilePath);
+  return await new Promise((resolve, reject) => {
+    webpack(
+      customConfig,
+      (err, stats) => {
+        if (err || stats.hasErrors()) reject(err)
+
+        const statsJSON = stats.toJson();
+        resolve(statsJSON.entrypoints.main.assetsSize);
+      }
+    );
+  });
+}
+
+/**
+ * Gets the size of the Qt bundle directly from the Glean folder.
+ *
+ * Note: this function assumes the Qt bundle has been built over there.
+ *
+ * @returns The size in bytes of the Qt bundle.
+ */
+export function getQtBundleSize() {
+  // The Qt bundle size is special, because the Qt build is not part of the @mozilla/glean npm package.
+  // It is already bundled and distributed with each new release on the Github releases page.
+  //
+  // In Qt, the bundle always contains all metric types and plugins, so there is no min/max.
+  return fs.statSync(path.resolve(__dirname, "../../glean/dist/qt/org/mozilla/Glean/glean.lib.js")).size;
+}
