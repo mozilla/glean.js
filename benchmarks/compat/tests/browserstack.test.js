ffd5bd203714e768d6560c9cce8e28545dd91192
brizental <beatriz.rizental@gmail.com>
2022-01-27 11:54:16 +0100

    Add cross browser test to browser compat benchmarks

---
+++ benchmarks/compat/tests/browserstack.test.js
@@ -0,0 +1,88 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import browserstack from "browserstack-local";
+import webdriver from "selenium-webdriver";
+
+import { runWebTest, fastSelenium } from "./utils.js"
+
+fastSelenium();
+
+// All of the minimum supported versions were found throw manual testing.
+const BROWSERS = [
+  { name: "firefox", minVersion: "60" },
+  { name: "safari", minVersion: "11.1" },
+  { name: "chrome", minVersion: "66" },
+  // Oldest version of edge available in BrowserStack.
+  // We are not sure it does not work in even older versions.
+  { name: "edge", minVersion: "80" },
+];
+
+// Start BrowserStack.Local so that the remote machines can access  localhost.
+const bs = new browserstack.Local();
+await new Promise(resolve => {
+  bs.start(
+    {
+      "key": process.env.BROWSERSTACK_ACCESS_KEY,
+      "forceLocal": "true",
+    },
+    () => {
+      console.log("Started BrowserStack.");
+      resolve();
+    }
+  );
+});
+
+// Run the test for both the latest version
+// and the minimum supported version of each browser.
+for (const browser of BROWSERS) {
+  for (const version of [ browser.minVersion, "latest" ]) {
+    console.info(`Smoke testing @mozilla/glean/web in ${browser.name} (${version}).`);
+    await (async function () {
+      let driver;
+      try {
+        const capabilities = {
+          "browserName" : browser.name,
+          "browser_version" : version,
+          "name": "Browser compatibility smoke test",
+          "build": process.env.CIRCLE_JOB || "Local BrowserStack testing",
+          "browserstack.networkLogs": "true",
+          "browserstack.debug": "true",
+          "browserstack.console" : "verbose",
+          "browserstack.local" : "true",
+        }
+
+        driver = new webdriver.Builder()
+          .usingServer(`https://${process.env.BROWSERSTACK_USER_NAME}:${process.env.BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub`)
+          .withCapabilities(capabilities)
+          .build();
+
+        await runWebTest(driver);
+
+        // Marking the test as passed for Browserstack.
+        await driver.executeScript(
+          "browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"passed\",\"reason\": \"Ping successfully sent!\"}}"
+        );
+      } catch(_) {
+        await driver.executeScript(
+          "browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\",\"reason\": \"Ping not sent :(\"}}"
+        );
+      }
+
+      // Quit the browser
+      await driver.quit();
+    }());
+  }
+}
+
+// Stop BrowserStack.Local.
+await new Promise(resolve => {
+  bs.stop(
+    () => {
+      console.log("Stopped BrowserStack.");
+      resolve();
+    }
+  );
+});
+
--- benchmarks/compat/tests/utils.js
+++ benchmarks/compat/tests/utils.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import http from "http";
+import https from "https";
+
 import { By, until } from "selenium-webdriver";
 
 /**
@@ -30,5 +33,43 @@ export async function runWebTest(driver) {
     console.log("Test failed.");
     // Bubble error up to calling function.
     throw e;
+  }
+}
+
+// Copied verbatim from:
+// https://www.browserstack.com/docs/automate/selenium/error-codes/keep-alive-not-used
+export function fastSelenium() {
+  //set the time (in seconds) for connection to be alive
+  var keepAliveTimeout = 30*1000;
+
+  if(http.globalAgent && http.globalAgent.hasOwnProperty('keepAlive')) {
+      http.globalAgent.keepAlive = true;
+      https.globalAgent.keepAlive = true;
+      http.globalAgent.keepAliveMsecs = keepAliveTimeout;
+      https.globalAgent.keepAliveMsecs = keepAliveTimeout;
+  } else {
+      var agent = new http.Agent({
+          keepAlive: true,
+          keepAliveMsecs: keepAliveTimeout
+      });
+
+      var secureAgent = new https.Agent({
+          keepAlive: true,
+          keepAliveMsecs: keepAliveTimeout
+      });
+
+      var httpRequest = http.request;
+      var httpsRequest = https.request;
+
+      http.request = function(options, callback){
+          if(options.protocol == "https:"){
+              options["agent"] = secureAgent;
+              return httpsRequest(options, callback);
+          }
+          else {
+              options["agent"] = agent;
+              return httpRequest(options, callback);
+          }
+      };
   }
 }
