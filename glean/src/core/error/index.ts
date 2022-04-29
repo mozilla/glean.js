6a992a8a2aaa98139f2bef6fc49ae49d320f46fa
unknown <tawahpeggy98@gmail.com>
2022-04-29 14:12:25 +0100

    Bug1761485-expose-ErrorType-through-its-own-entry-point

--- glean/src/core/error/index.ts
+++ glean/src/core/error/index.ts
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import type { MetricType } from "../metrics/index.js";
-import { ErrorType } from "@mozilla/glean/error";
+import { ErrorType } from "./error_type.js";
 import { InternalCounterMetricType as CounterMetricType} from "../metrics/types/counter.js";
 import { combineIdentifierAndLabel, stripLabel } from "../metrics/types/labeled.js";
 import log from "../log.js";
