3a071fbd56d0453f7874ad7695c6f9b384abf5b0
Bruno Rosa <rosahbruno@gmail.com>
2024-03-31 22:23:54 -0400

    automatic page loads
    changelog
    - create custom PageTitle component with telemetry built in
    - update astro config for new skeleton + component override

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -5,17 +5,39 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Glean.js',
+			components: {
+        // Override the default `SocialIcons` component.
+        PageTitle: './src/components/CustomPageTitle.astro',
+      },
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/mozilla/glean.js',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'About',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Overview', link: '/overview' },
+						{ label: 'Demo', link: '/demo' },
+						{ label: 'FAQ', link: '/faq' },
+						{
+							label: 'Roadmap',
+							link: 'https://docs.google.com/document/d/1vqu9O9VRGRiuptQbGPY5UX-cH2gkUSVj4EoZizQtHGM/edit#heading=h.m18zbbjlaigo',
+							attrs: { target: '_blank', style: 'font-style: italic' }
+						},
 					],
+				},
+				{
+					label: 'Getting Started',
+					items: [
+						{
+							label: 'Adding Glean to your project',
+							link: 'https://mozilla.github.io/glean/book/user/adding-glean-to-your-project/index.html',
+							attrs: { target: '_blank', style: 'font-style: italic' }
+						},
+						{ label: 'Integration Guide', link: '/guides/integration' },
+					]
 				},
 				{
 					label: 'Reference',
