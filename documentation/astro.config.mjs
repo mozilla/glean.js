8d3546e4176ba6f264a52a7f0647673adf260d47
Bruno Rosa <rosahbruno@gmail.com>
2024-04-21 23:45:25 -0400

    project config

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -15,18 +15,15 @@ export default defineConfig({
       github: 'https://github.com/mozilla/glean.js'
     },
     sidebar: [{
-      label: 'About',
+      label: 'Introduction',
       items: [
       // Each item here is one entry in the navigation menu.
       {
         label: 'Overview',
-        link: '/overview'
+        link: '/'
       }, {
-        label: 'Demo',
-        link: '/demo'
-      }, {
-        label: 'FAQ',
-        link: '/faq'
+        label: 'Interactive playground',
+        link: '/playground'
       }, {
         label: 'Roadmap',
         link: 'https://docs.google.com/document/d/1vqu9O9VRGRiuptQbGPY5UX-cH2gkUSVj4EoZizQtHGM/edit#heading=h.m18zbbjlaigo',
@@ -38,17 +35,40 @@ export default defineConfig({
     }, {
       label: 'Getting Started',
       items: [{
-        label: 'Adding Glean to your project',
-        link: 'https://mozilla.github.io/glean/book/user/adding-glean-to-your-project/index.html',
+        label: 'Integration guide',
+        link: 'https://mozilla.github.io/glean/book/user/adding-glean-to-your-project/javascript.html',
         attrs: {
           target: '_blank',
           style: 'font-style: italic'
         }
       }, {
-        label: 'Integration Guide',
-        link: '/guides/integration'
+        label: 'Platforms',
+        link: '/getting_started/platforms'
       }]
     }, {
+      label: 'Automatic Events',
+      items: [{
+        label: 'Why',
+        link: '/automatic_instrumentation/why'
+      }, {
+        label: 'Page load events',
+        link: '/automatic_instrumentation/page_load_events'
+      }, {
+        label: 'Click events',
+        link: '/automatic_instrumentation/click_events'
+      }]
+    }, {
+      label: 'Debugging',
+      items: [{
+        label: 'Options',
+        link: '/debugging/options'
+      }, {
+        label: 'Browser debugging',
+        link: '/debugging/browser'
+      }]
+    }, {
+      // This auto generates a list for all md files in the
+      // `/documentation/src/content/docs/reference` directory.
       label: 'Reference',
       autogenerate: {
         directory: 'reference'
